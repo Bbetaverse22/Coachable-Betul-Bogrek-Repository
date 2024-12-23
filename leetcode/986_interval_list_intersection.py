15f007ba728a0bbd44562d0871a6b7d4fd8fcd47
Betul <betulmath@Betuls-MacBook-Pro.local>
2024-12-23 14:59:24 -0600

    pylint fix

--- leetcode/986_interval_list_intersection.py
+++ leetcode/986_interval_list_intersection.py
@@ -5,16 +5,10 @@ from typing import List
 class Solution:
     """A class used to find the intersections of two interval lists."""
 
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]
+    ) -> List[List[int]]:
         """
         Returns the intersection of two interval lists.
-        
-        Args:
-            firstList (List[List[int]]): The first list of intervals.
-            secondList (List[List[int]]): The second list of intervals.
-        
-        Returns:
-            List[List[int]]: The list of intersecting intervals.
         """
         i, j = 0, 0
         result = []
