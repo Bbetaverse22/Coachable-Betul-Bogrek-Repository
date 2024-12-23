3f35f5902b3bbf8c29a78ea96125fb4ab7f8c711
Betul <betulmath@Betuls-MacBook-Pro.local>
2024-12-23 15:16:12 -0600

    Pylint fix

--- leetcode/721_accounts_merge.py
+++ leetcode/721_accounts_merge.py
@@ -5,25 +5,23 @@ from collections import defaultdict
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         """
-        Given a list of accounts where each account is a list of strings, this function merges the accounts.
-        Each account contains a name and a list of emails. Two accounts are considered to be the same if they
-        have at least one email in common. The function returns the merged accounts in the format [name, email1, email2, ...].
+        Given a list of accounts where each account is a list of strings,
+        this function merges the accounts.
         """
         parent = {}
         email_to_name = {}
-        
+
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
             return parent[x]
-        
+
         def union(x, y):
             rootX = find(x)
             rootY = find(y)
             if rootX != rootY:
                 parent[rootY] = rootX
-        
-        # Initialize the union-find structure
+
         for account in accounts:
             name = account[0]
             for email in account[1:]:
@@ -31,16 +29,14 @@ class Solution:
                     parent[email] = email
                 email_to_name[email] = name
                 union(account[1], email)
-        
-        # Group emails by their root parent
+
         merged_accounts = defaultdict(list)
         for email in parent:
             root = find(email)
             merged_accounts[root].append(email)
-        
-        # Format the result
+
         result = []
         for emails in merged_accounts.values():
             result.append([email_to_name[emails[0]]] + sorted(emails))
-        
+
         return result
