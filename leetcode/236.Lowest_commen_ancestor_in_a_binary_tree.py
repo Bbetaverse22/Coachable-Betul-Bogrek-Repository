f96bc43437c1362923149a69c6cb1de5a407004d
Betul <betulmath@Betuls-MacBook-Pro.local>
2024-12-23 13:15:07 -0600

    236 Lowest common ancestor of a binary tree

---
+++ leetcode/236.Lowest_commen_ancestor_in_a_binary_tree.py
@@ -0,0 +1,20 @@
+"""236 Lowest Common Ancestor of a Binary Tree"""
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """Find the lowest common ancestor of two nodes in a binary tree."""
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
