7a178a22943acbb742a4acaf81ae330395cb0650
Betul <betulmath@Betuls-MacBook-Pro.local>
2024-12-28 17:52:02 -0600

    pylint fix

--- leetcode/825_friends_of_appropriate_ages.py
+++ leetcode/825_friends_of_appropriate_ages.py
@@ -9,13 +9,13 @@ class Solution:
         This function counts the number of friend requests made based on age constraints.
         """
         ages.sort()
-        
+
         freq = [0] * 121
         for age in ages:
             freq[age] += 1
-        
+
         total_requests = 0
-        
+
         for age_a in range(1, 121):
             if freq[age_a] == 0:
                 continue
@@ -29,5 +29,5 @@ class Solution:
                 if age_b > 100 and age_a < 100:
                     continue
                 total_requests += freq[age_a] * (freq[age_b] - (age_a == age_b))
-        
+
         return total_requests
