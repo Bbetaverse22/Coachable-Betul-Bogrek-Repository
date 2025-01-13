b9520b6dc612d6b03e43c0014815f15a2743919a
Betul <betulmath@Betuls-MacBook-Pro.local>
2025-01-12 21:01:16 -0600

    using reservoir sampling

--- leetcode/398_random_pick_index.py
+++ leetcode/398_random_pick_index.py
@@ -20,3 +20,16 @@ class Solution:
             index = random.randint(0, len(self.nums) - 1)
             if target == self.nums[index]:
                 return index
+
+    def pick_reservoir_sampling(self, target: int) -> int:
+        """
+        Picks a random index for the given target number using Reservoir Sampling.
+        """
+        count = 0
+        result = -1
+        for i, num in enumerate(self.nums):
+            if num == target:
+                count += 1
+                if random.randint(1, count) == 1:
+                    result = i
+        return result
