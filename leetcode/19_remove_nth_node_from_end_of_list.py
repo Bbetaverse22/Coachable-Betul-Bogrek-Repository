780dfb9f22e80b01a580b9ab929d9c844c95af91
Betul <betulmath@Betuls-MacBook-Pro.local>
2025-01-12 21:34:18 -0600

    use fast and slow pointers

--- leetcode/19_remove_nth_node_from_end_of_list.py
+++ leetcode/19_remove_nth_node_from_end_of_list.py
@@ -14,17 +14,17 @@ class Solution:
         This function removes the nth node from the end of the list and returns the head of the modified list.
         """
         dummy = ListNode(0, head)
-        first = dummy
-        second = dummy
+        fast = dummy
+        slow = dummy
 
         for _ in range(n + 1):
-            first = first.next
+            fast = fast.next
 
-        while first:
-            first = first.next
-            second = second.next
+        while fast:
+            fast = fast.next
+            slow = slow.next
 
-        second.next = second.next.next
+        slow.next = slow.next.next
 
         return dummy.next
 
