f700a6389fca6854ba59388b8e57966d8af8d759
Betul <betulmath@Betuls-MacBook-Pro.local>
2025-02-09 22:14:14 -0600

    735 asteroid collision

---
+++ leetcode/735_asteroid_collision.py
@@ -0,0 +1,21 @@
+"""Asteroid Collision"""
+from typing import List
+
+class Solution:
+
+    def asteroid_collision(self, asteroids: List[int]) -> List[int]:
+        """
+        Simulates the collision of asteroids coming from left to right.
+        """
+        stack = []
+        for ast in asteroids:
+            while stack and ast < 0 < stack[-1]:
+                if stack[-1] < -ast:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -ast:
+                    stack.pop()
+                break
+            else:
+                stack.append(ast)
+        return stack
